04402da6226422028281897b3c3c8e96f4e77546
Markish <markishh@gmail.com>
2022-02-12 11:14:44 +0530

    oracleOpen.js

--- web3/oracleopen.js
+++ web3/oracleopen.js
@@ -1,14 +1,15 @@
 const { diamondAddress } = require('../constants/constants');
 const OpenOracleContract = require('../blockchain/abis/OracleOpen.json');
+const LoanExt =require('../blockchain/abis/LoanExt.json');
 const { sendTransaction, getWeb3, getValue } = require('./transaction');
 
 const liquidationTrigger = (address, loanId) => {
     const web3 = getWeb3();
-    let oracleOpenContract = new web3.eth.Contract(
-        OpenOracleContract.abi,
+    let LoanExtContract = new web3.eth.Contract(
+        LoanExt,
         diamondAddress
     );
-    return sendTransaction(diamondAddress, oracleOpenContract, "liquidationTrigger", address, loanId);
+    return sendTransaction(diamondAddress, LoanExtContract, "liquidationTrigger", address, loanId);
 }
 
 const setFairPrice = (requestId, fairPrice, market, amount) => {
